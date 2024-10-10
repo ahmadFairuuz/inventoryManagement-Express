bb95697eb1a1b6a27543c4b7152c50ff2194ce17
ahmadFairuuz <ahmadfruz@gmail.com>
2024-10-10 23:25:39 +0700

    add API Item, User, Transaction

--- src/app.js
+++ src/app.js
@@ -3,22 +3,22 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
-const authController = require("./auth/auth.controller");
-
 app.use(express.json());
 
-// process.on("uncaughtException", function (err) {
-//   console.log(err);
-// });
+app.get("/", (req, res) => {
+  res.send("Sever Working");
+});
+
+const authController = require("./auth/auth.controller");
+const itemController = require("./item/item.controller");
+const userController = require("./user/user.controller");
+const transactionController = require("./transaction/transaction.controller");
 
 app.use("/api/auth", authController);
+app.use("/api/items", itemController);
+app.use("/api/users", userController);
+app.use("/api/transaction", transactionController);
 
-app.get("/", (req, res) => {
-  res.send("Hello there!");
-});
-
-
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
-
