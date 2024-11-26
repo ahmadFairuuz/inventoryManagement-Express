8a8c9ead450ffbf2cb22e1057b88e8cda5011248
ahmadFairuuz <ahmadfruz@gmail.com>
2024-11-26 20:10:24 +0700

    integrasi dengan frontend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Sever Working");
@@ -17,9 +19,8 @@ const adminAuthorization = require("./middleware/adminAuthorization");
 
 app.use("/api/auth", authController);
 app.use("/api/items", itemController);
-app.use("/api/users", userController);
 app.use("/api/transaction", transactionController);
-app.use("/api/users", adminAuthorization, userController);
+app.use("/api/users", userController);
 
 app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running on http://127.0.0.1:${PORT}`);
